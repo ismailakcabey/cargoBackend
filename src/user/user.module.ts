251bc55c9406a4c637596e3858c2d969b6ae5f8c
ismailakca <ismailakca399@gmail.com>
2022-11-09 18:57:49 +0300

    fix exprenses and user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,15 +2,17 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserExpensesController } from "src/userExpenses/userExpenses.controller";
 import { UserExpensesService } from "src/userExpenses/userExpenses.service";
+import { UsersController } from "./user.controller";
 import { UserSchema } from "./user.model";
+import { UserService } from "./user.service";
 
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{name:"UserExpenses",schema:UserSchema}])
+        MongooseModule.forFeature([{name:"User",schema:UserSchema}])
     ],
-    controllers:[UserExpensesController],
-    providers:[UserExpensesService]
+    controllers:[UsersController],
+    providers:[UserService]
 })
 
 export class UserModule { }
--- src/userExpenses/userExpenses.module.ts
+++ src/userExpenses/userExpenses.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { UserExpensesController } from "./userExpenses.controller";
 import { UserExpensesSchema } from "./userExpenses.model";
+import { UserExpensesService } from "./userExpenses.service";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{name:"UserExpenses",schema:UserExpensesSchema}])
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserExpensesController],
+  providers: [UserExpensesService],
   
 })
 
