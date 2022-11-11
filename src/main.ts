fe3b31b5879d674a0becb64af39a91f130d2b3ae
ismailakca <ismailakca399@gmail.com>
2022-11-11 22:56:14 +0300

    log

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env.PORT || 3000;
+  console.log("successfully started")
   await app.listen(port);
 }
 bootstrap();
